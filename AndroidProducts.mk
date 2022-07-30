7aff125f5ebdcbfc917b8496934e604dc8c4d245
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-30 16:43:23 +0800

    [TEMP] mi439: Add back Lineage product makefile
    Note that rro_overlays/*_lineage should be dropped after that, They're Lineage-specific.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     aosp_mi439-user \
     aosp_mi439-userdebug \
     aosp_mi439-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/lineage_mi439.mk
+
+COMMON_LUNCH_CHOICES += \
+    lineage_mi439-user \
+    lineage_mi439-userdebug \
+    lineage_mi439-eng
